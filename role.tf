8b0504cc245241f69871c439db9ac574ee77084d
Ubuntu <ubuntu@ip-172-31-81-68.ec2.internal>
2020-04-14 02:37:25 +0000

    ajuste nomes

--- role.tf
+++ role.tf
@@ -1,5 +1,5 @@
 resource "aws_iam_role"  "ecr_readOnly_role" {
-  name_prefix = terraform.workspace
+  name_prefix = "${format("ecr_readOnly_role_%s_", terraform.workspace)}"
   assume_role_policy = <<EOF
 {
   "Version": "2012-10-17",
@@ -22,12 +22,12 @@ EOF
 }
 
 resource "aws_iam_instance_profile" "ecr_readOnly_profile" {
-  name_prefix = terraform.workspace
+  name_prefix = "${format("ecr_readOnly_profile_%s", terraform.workspace)}"
   role = "${aws_iam_role.ecr_readOnly_role.name}"
 }
 
 resource "aws_iam_role_policy" "ecr_readOnly_policy" {
-  name_prefix = terraform.workspace
+  name_prefix = "${format("ecr_readOnly_policy_%s", terraform.workspace)}"
   role = "${aws_iam_role.ecr_readOnly_role.id}"
 
   policy = <<EOF
