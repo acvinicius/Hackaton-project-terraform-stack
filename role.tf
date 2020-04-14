4aa900f3aa52ee3f0866cadc74b5495ad9a668cc
Ubuntu <ubuntu@ip-172-31-81-68.ec2.internal>
2020-04-14 01:22:42 +0000

    ajsute role

--- role.tf
+++ role.tf
@@ -1,5 +1,5 @@
 resource "aws_iam_role"  "ecr_readOnly_role" {
-  name = "${format("ecr_readOnly_role_%s", terraform.workspace)}"
+  name = "${format("ecr_readOnly_role_%s_%03d", terraform.workspace, count.index + 1)}"
   assume_role_policy = <<EOF
 {
   "Version": "2012-10-17",
@@ -17,17 +17,17 @@ resource "aws_iam_role"  "ecr_readOnly_role" {
 EOF
 
   tags = {
-      Name = "${format("ecr_readOnly_role_%s", terraform.workspace)}"
+      Name = "${format("ecr_readOnly_role_%s_%03d", terraform.workspace, count.index + 1)}"
   }
 }
 
 resource "aws_iam_instance_profile" "ecr_readOnly_profile" {
-  name = "${format("ecr_readOnly_profile_%s", terraform.workspace)}"
+  name = "${format("ecr_readOnly_profile_%s_%03d", terraform.workspace, count.index + 1)}"
   role = "${aws_iam_role.ecr_readOnly_role.name}"
 }
 
 resource "aws_iam_role_policy" "ecr_readOnly_policy" {
-  name = "${format("ecr_readOnly_policy_%s", terraform.workspace)}"
+  name = "${format("ecr_readOnly_policy_%s_%03d", terraform.workspace, count.index + 1)}"
   role = "${aws_iam_role.ecr_readOnly_role.id}"
 
   policy = <<EOF
